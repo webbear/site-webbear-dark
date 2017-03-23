b117ba238cc7aab927d4b29758c7a094a507f2e4
Hans-Jakob Bollinger <contact@webbear.ch>
2017-03-23 08:08:13 +0100

    1. commit

---
+++ install/info.php
@@ -0,0 +1,6 @@
+<?php if(!defined("PROCESSWIRE_INSTALL")) die();
+$info = array(
+	'title' => "webbears dark site profile", 
+	'summary' => "webbears dark site profile", 
+	'screenshot' => "2017-03-23_07-49-50-site-profile-dark.png"
+	);
