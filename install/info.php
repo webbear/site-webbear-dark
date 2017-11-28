6f1074e256c47ffc53a79700e10e325139f87aee
Hans Jakob Bollinger <contact@webbear.ch>
2017-11-28 07:32:23 +0100

    updated to webbear-sass-2, updated wire to latest, remove unused modules

--- install/info.php
+++ install/info.php
@@ -1,6 +1,6 @@
 <?php if(!defined("PROCESSWIRE_INSTALL")) die();
 $info = array(
 	'title' => "webbears dark Site Profile", 
-	'summary' => "", 
-	'screenshot' => "site-profile-dark.png"
+	'summary' => "webbears dark site profile for pw driven websites", 
+	'screenshot' => "site-profile-dark.jpg"
 	);
