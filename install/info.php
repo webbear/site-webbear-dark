4189c91277dd44a354369c9810e6a5056c7eb32e
Hans Jakob Bollinger <contact@webbear.ch>
2018-05-31 07:33:25 +0200

    sass and php updates

--- install/info.php
+++ install/info.php
@@ -1,6 +1,6 @@
 <?php if(!defined("PROCESSWIRE_INSTALL")) die();
 $info = array(
-	'title' => "webbears dark Site Profile", 
-	'summary' => "webbears dark site profile for pw driven websites", 
-	'screenshot' => "site-profile-dark.jpg"
+	'title' => "Webbears Dark Site Profile", 
+	'summary' => "", 
+	'screenshot' => "screenshot_40.png"
 	);
